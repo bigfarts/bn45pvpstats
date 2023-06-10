2705e683d9b18606d9a0d6bc2eeb90857f526f1b
bigfarts <bigfarts@punymail.com>
2023-06-09 21:59:16 -0700

    log errors

--- src/bin/patchessyncserver.rs
+++ src/bin/patchessyncserver.rs
@@ -2,6 +2,76 @@ use futures::StreamExt;
 use itertools::Itertools;
 use tokio::io::AsyncWriteExt;
 
+async fn run_once() -> Result<(), anyhow::Error> {
+    let client = reqwest::Client::new();
+    let entries = tokio::time::timeout(
+        // 30 second timeout to fetch JSON.
+        std::time::Duration::from_secs(30),
+        (|| async {
+            Ok::<_, anyhow::Error>(
+                client
+                    .get("https://patches.tango.n1gp.net/index.json")
+                    .header("User-Agent", "tango")
+                    .send()
+                    .await?
+                    .json::<tango_filesync::Entries>()
+                    .await?,
+            )
+        })(),
+    )
+    .await??
+    .into_iter()
+    .filter(|(k, _)| k == "exe45_pvp" || k == "bn45_us_pvp")
+    .collect::<std::collections::HashMap<String, tango_filesync::Entry>>();
+
+    tango_filesync::sync(
+        &root,
+        &entries,
+        {
+            let root = root.clone();
+            move |path| {
+                let root = root.clone();
+                Box::pin(async move {
+                    let mut output_file = tokio::fs::File::create(&root.join(path)).await?;
+                    let client = reqwest::Client::new();
+                    let mut stream = tokio::time::timeout(
+                        // 30 second timeout to initiate connection.
+                        std::time::Duration::from_secs(30),
+                        client
+                            .get(format!(
+                                "https://patches.tango.n1gp.net/{}",
+                                path.components()
+                                    .map(|v| v.as_os_str().to_string_lossy())
+                                    .join("/")
+                            ))
+                            .header("User-Agent", "tango")
+                            .send(),
+                    )
+                    .await?
+                    .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?
+                    .bytes_stream();
+                    while let Some(chunk) = tokio::time::timeout(
+                        // 30 second timeout per stream chunk.
+                        std::time::Duration::from_secs(30),
+                        stream.next(),
+                    )
+                    .await?
+                    {
+                        let chunk =
+                            chunk.map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
+                        output_file.write_all(&chunk).await?;
+                    }
+                    log::info!("filesynced: {}", path.display());
+                    Ok(())
+                })
+            }
+        },
+        4,
+    )
+    .await?;
+    Ok(())
+}
+
 #[tokio::main]
 async fn main() -> Result<(), anyhow::Error> {
     env_logger::Builder::from_default_env()
@@ -12,75 +82,12 @@ async fn main() -> Result<(), anyhow::Error> {
     std::fs::create_dir_all(root)?;
 
     loop {
-        let client = reqwest::Client::new();
-        let entries = tokio::time::timeout(
-            // 30 second timeout to fetch JSON.
-            std::time::Duration::from_secs(30),
-            (|| async {
-                Ok::<_, anyhow::Error>(
-                    client
-                        .get("https://patches.tango.n1gp.net/index.json")
-                        .header("User-Agent", "tango")
-                        .send()
-                        .await?
-                        .json::<tango_filesync::Entries>()
-                        .await?,
-                )
-            })(),
-        )
-        .await??
-        .into_iter()
-        .filter(|(k, _)| k == "exe45_pvp" || k == "bn45_us_pvp")
-        .collect::<std::collections::HashMap<String, tango_filesync::Entry>>();
-
-        tango_filesync::sync(
-            &root,
-            &entries,
-            {
-                let root = root.clone();
-                move |path| {
-                    let root = root.clone();
-                    Box::pin(async move {
-                        let mut output_file = tokio::fs::File::create(&root.join(path)).await?;
-                        let client = reqwest::Client::new();
-                        let mut stream = tokio::time::timeout(
-                            // 30 second timeout to initiate connection.
-                            std::time::Duration::from_secs(30),
-                            client
-                                .get(format!(
-                                    "https://patches.tango.n1gp.net/{}",
-                                    path.components()
-                                        .map(|v| v.as_os_str().to_string_lossy())
-                                        .join("/")
-                                ))
-                                .header("User-Agent", "tango")
-                                .send(),
-                        )
-                        .await?
-                        .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?
-                        .bytes_stream();
-                        while let Some(chunk) = tokio::time::timeout(
-                            // 30 second timeout per stream chunk.
-                            std::time::Duration::from_secs(30),
-                            stream.next(),
-                        )
-                        .await?
-                        {
-                            let chunk = chunk
-                                .map_err(|e| std::io::Error::new(std::io::ErrorKind::Other, e))?;
-                            output_file.write_all(&chunk).await?;
-                        }
-                        log::info!("filesynced: {}", path.display());
-                        Ok(())
-                    })
-                }
-            },
-            4,
-        )
-        .await?;
+        if let Err(err) = run_once().await {
+            log::error!("patch sync error: {}", err);
+        }
 
         const SLEEP_DURATION: std::time::Duration = std::time::Duration::from_secs(5 * 60);
-        log::info!("done, sleeping for {:?}", SLEEP_DURATION);
+        log::info!("sleeping for {:?}", SLEEP_DURATION);
         tokio::time::sleep(SLEEP_DURATION).await;
     }
 }
